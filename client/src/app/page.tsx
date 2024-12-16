b469bc4ff4615afa32241831c04532459ef9f1b2
ankit <sahuankit.murhu@gmail.com>
2024-12-17 00:04:59 +0530

    fixing responsivness

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -2,12 +2,12 @@ import Container from "@/components/Container";
 
 export default function Home() {
   return (
-    <div className="h-screen w-full bg-darkblue">
-      <div className="h-[25%]">
-        <h1 className="text-6xl text-center pt-16 font-extrabold text-lemon">
+    <div className="h-full w-full bg-darkblue sm:flex sm:flex-col sm:justify-center sm:items-center sm:gap-20"> 
+      <div className="pb-5">
+        <h1 className="text-6xl sm:text-8xl text-center pt-5 font-extrabold text-lemon">
           s<span className="text-zinc-200">z</span>are
         </h1>
-        <h2 className="text-xl text-zinc-200 text-center">file sharing</h2>
+        <h2 className="text-xl sm:text-2xl text-zinc-200 text-center">file sharing</h2>
       </div>
       <Container />
     </div>
