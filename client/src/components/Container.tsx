c2a0f2a176dd4924bdd77797264e311bd076f299
ankit <sahuankit.murhu@gmail.com>
2024-12-17 07:08:25 +0530

    Responsive adjustments

--- client/src/components/Container.tsx
+++ client/src/components/Container.tsx
@@ -3,7 +3,7 @@ import Directories from "./Directories";
 export default function Container() {
   return (
     <div className=" flex justify-center items-center w-full">
-      <div className="h-[100%] bg-[#0a2567] w-10/12 rounded-md sm:w-8/12 md:w-6/12 lg:w-4/12 xl:w-3/12">
+      <div className="h-[100%] bg-[#0a2567] w-10/12 rounded-md sm:w-8/12 md:w-8/12 lg:w-8/12 xl:w-8/12">
         <Directories />
       </div>
     </div>
