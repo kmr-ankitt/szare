3af2541d4ccc7212c7ccca69434dfe358793ed58
ankit <sahuankit.murhu@gmail.com>
2024-12-18 20:51:36 +0530

    Testing

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Downloading Szare..."
-curl -L -o szare https://www.github.com/kmr-ankitt/szare
+curl -L -o szare https://www.github.com/kmr-ankitt/szare/szare
 chmod +x szare
 echo "Starting Szare..."
 ./szare
